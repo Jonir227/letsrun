2a35dbc0159e64a0e6ffc99f59eb084d2ed934f5
Kim Bum June <bjbj6363@gmail.com>
2019-09-21 17:19:07 +0900

    initial commit

---
+++ pages/about.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const About = () => {
+  return <Layout>it's me</Layout>;
+};
+
+export default About;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout";
+
+const Index = () => {
+  return <Layout>hi</Layout>;
+};
+
+export default Index;
