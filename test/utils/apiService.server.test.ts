66c58ea96766cb57d827f3b3f45966a0895dfe35
Kim Bum June <bjbj6363@gmail.com>
2019-09-27 16:46:01 +0900

    테스트 추가, jest 와치하도록 변경

---
+++ test/utils/apiService.server.test.ts
@@ -0,0 +1,53 @@
+/**
+ * @jest-environment node
+ */
+
+jest.mock("axios");
+
+import axios from "axios";
+import apiSerivce from "../../utils/apiService";
+
+const url = "/api/request";
+
+describe("ApiSerivce", () => {
+  test("should be request with proper url using GET", async () => {
+    // @ts-ignore
+    axios.get.mockReturnValue(Promise.resolve(1));
+
+    const data = await apiSerivce.get(url);
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith(`http://localhost:3000${url}`);
+    expect(data).toBe(1);
+  });
+  test("should be request with proper url using POST", async () => {
+    // @ts-ignore
+    axios.post.mockReturnValue(Promise.resolve({ age: 27 }));
+
+    const data = await apiSerivce.post(url, { name: "john" });
+    expect(axios.post).toHaveBeenCalledTimes(1);
+    expect(axios.post).toHaveBeenCalledWith(`http://localhost:3000${url}`, {
+      name: "john"
+    });
+    expect(data).toEqual({ age: 27 });
+  });
+  test("should be request with proper url using PUT", async () => {
+    // @ts-ignore
+    axios.put.mockReturnValue({ success: true });
+
+    const data = await apiSerivce.put(url);
+    expect(axios.put).toHaveBeenCalledTimes(1);
+    expect(axios.put).toHaveBeenCalledWith(`http://localhost:3000${url}`);
+    expect(data).toEqual({ success: true });
+  });
+  test("should be request with proper url using DELETE", async () => {
+    const returnValue = "success";
+    // @ts-ignore
+    axios.delete.mockReturnValue(Promise.resolve(returnValue));
+
+    const data = await apiSerivce.delete(url);
+    expect(axios.delete).toHaveBeenCalledTimes(1);
+    expect(axios.delete).toHaveBeenCalledWith(`http://localhost:3000${url}`);
+    expect(data).toBe(returnValue);
+  });
+});
---
+++ test/utils/getCurrentUrl.server.test.ts
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment node
+ */
+
+import getCurrentUrl from "../../utils/getCurrentUrl";
+
+describe("GetCurrentUrl in Server", () => {
+  test("Should be return full location when its called in server", () => {
+    const url = "/api/path";
+    expect(getCurrentUrl(url)).toBe("http://localhost:3000/api/path");
+  });
+  test("Should be return proper port when it's provided", () => {
+    process.env.PORT = "8088";
+    const url = "/api/path";
+    expect(getCurrentUrl(url)).toBe("http://localhost:8088/api/path");
+  });
+});
