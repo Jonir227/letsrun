d71e49ae0dadf286369e0189a13250e8cedf43a5
Kim Bum June <bjbj6363@gmail.com>
2019-09-30 05:51:33 +0900

    get horseInfo api 추가

--- api/horseInfo.ts
+++ api/horseInfo.ts
@@ -1,6 +1,6 @@
 import apiService from "../utils/apiService";
 
 export const getHorseInfo = async (horseId: string) => {
-  const data = apiService.get("/api/horses");
+  const data = apiService.get(`/api/horses/${horseId}`);
   return data;
 };
