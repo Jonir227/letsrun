254a93d3db03b9604066b249726d82887c2b8282
Kim Bum June <bjbj6363@gmail.com>
2019-10-12 23:06:53 +0900

    오타 수정

--- src/pages/horse.tsx
+++ src/pages/horse.tsx
@@ -1,30 +1,28 @@
-import { NextJSContext } from "next-redux-wrapper";
-import React from "react";
-import { useSelector } from "react-redux";
-import Layout from "../components/Layout";
-import { getHorseProfile } from "../redux/actions/horseProfileActions";
-import { GET_HORSE_PROFILE_ENTITY } from "../redux/actionTypes";
-import { IRootStore } from "../redux/reducers";
-import { IHorseProfileStore } from "../redux/reducers/horseInfo";
-import { NextReduxPage } from "../typings";
-import { wait } from "../utils/reduxSagaSSRMiddleware";
+import { NextJSContext } from 'next-redux-wrapper';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Layout from '../components/Layout';
+import { getHorseProfile } from '../redux/actions/horseProfileActions';
+import { GET_HORSE_PROFILE_ENTITY } from '../redux/actionTypes';
+import { IRootStore } from '../redux/reducers';
+import { IHorseProfileStore } from '../redux/reducers/horseInfo';
+import { NextReduxPage } from '../typings';
+import { parseHorseGameDate } from '../utils/horseUtil';
+import { wait } from '../utils/reduxSagaSSRMiddleware';
 
 const Horse: NextReduxPage<IHorseProfileStore, {}> = () => {
   const { horseProfile, getHorseInfoStatus } = useSelector(
     (store: IRootStore) => store.horseProfile
   );
-  if (getHorseInfoStatus === "REQUEST" || !horseProfile) {
-    return "로딩중임니다";
+  if (getHorseInfoStatus === 'REQUEST' || !horseProfile) {
+    return '로딩중임니다';
   }
   return (
     <Layout>
       <h1>
         {horseProfile.name}#{horseProfile.number}
       </h1>
-      <div>
-        경기 날짜: {horseProfile.gameDate[0]}년 {horseProfile.gameDate[1]}월{" "}
-        {horseProfile.gameDate[2]}일 ({horseProfile.day})
-      </div>
+      <div>경기 날짜: {parseHorseGameDate(horseProfile)}</div>
       <div>성별: {horseProfile.sex}</div>
       <div>소유주: {horseProfile.ownerName}</div>
     </Layout>
--- src/utils/horseUtil/horseUtil.test.ts
+++ src/utils/horseUtil/horseUtil.test.ts
@@ -1,12 +1,12 @@
 import { HorseProfile } from '../../models';
-import { parseHorseGameDate } from './HorseUtil';
+import { parseHorseGameDate } from './horseUtil';
 
 const testHorseProfile = {
   gameDate: [2019, 10, 12]
 } as HorseProfile;
 
 describe('Test Set of HorseUtil', () => {
-  test('getHorseDate sould be return proper parsed readable date', () => {
+  test('parseHorseGameDate should be return proper parsed readable date', () => {
     expect(parseHorseGameDate(testHorseProfile)).toBe('2019년 10월 12일');
   });
 });
